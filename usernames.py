f2b199a7ffcf2d0f13b06a5d6bc23a8e89d44dfd
ijaz ahmad khan <ijazahmad722@gmail.com>
2016-02-29 23:39:12 +0100

    usernames and ids script updated

--- usernames.py
+++ usernames.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
-f=open('/etc/passwd','r')
-d={}
+import sys
+
+try:
+  f = open('/etc/passwd','r')
+except Exception as e:
+  print e
+  sys.exit(1)
+
+users_and_ids = []
 for line in f:
   info = line.split(':')
-  user,id = info[0],int(info[3])
-  d[id] = user
-for key in sorted(d):
-  print key , d[key]
+  pair = int(info[3]),(info[0])
+  users_and_ids.append(pair)
+
+users_and_ids.sort(key=lambda k:k[0])
+for id,usr in users_and_ids:
+  print id,usr
