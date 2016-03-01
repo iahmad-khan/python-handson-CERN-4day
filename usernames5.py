6406927533255e7e478596db8154c04210046831
ijaz ahmad khan <ijazahmad722@gmail.com>
2016-03-01 14:06:07 +0100

    using lambda functions

---
+++ usernames5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+f = open('/etc/passwd','r')
+users_and_ids = []
+for line in f:
+  u,_,id,_ = line.split(':',3)
+  users_and_ids.append((u,int(id)))
+users_and_ids.sort(key = lambda k:k[1])
+for id,usr in users_and_ids:
+  print id,usr
+
+
