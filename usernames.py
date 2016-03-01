82e705549b2a5bb116f464e3f085b2354cc97e90
ijaz ahmad khan <ijazahmad722@gmail.com>
2016-03-01 10:54:09 +0100

    usernames changed

--- usernames.py
+++ usernames.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import sys
+def my_cmp(a, b):
+  return cmp(a[1], b[1])
+
 
 f = open('/etc/passwd','r')
 
@@ -7,9 +10,10 @@ users_and_ids = []
 
 for line in f:
   u,_,id,_ = line.split(':',3)
-  pair = int(id),u
-  users_and_ids.append(pair)
-
-users_and_ids.sort()
+  users_and_ids.append((u,int(id)))
+#using cmp
+#users_and_ids.sort(cmp=my_cmp)
+#using lambda
+users_and_ids.sort(key=lambda k:k[1])
 for id,usr in users_and_ids:
   print id,usr
