27beaef05cfb48382c03bb9d83e1a9f21566b20a
ijaz ahmad khan <ijazahmad722@gmail.com>
2016-03-01 16:16:03 +0100

    using map reduce in python

--- map_reduce.py
+++ map_reduce.py
@@ -2,5 +2,6 @@
 from operator import add
 data=open('input','r')
 for line in data:
- sum=reduce(add,map(int,line.split()))
- print sum
+  if line.rstrip():
+    sum=reduce(add,map(int,line.split()))
+    print sum
